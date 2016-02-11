ff1cb0bd2110a73a85bf02786b85b19baeb366d3
maple-nishiyama <d.nishiyama@maplesystems.co.jp>
2016-02-11 15:42:11 +0900

    Initial Commit

---
+++ neuralnet/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  neuralnet
+//
+//  Created by 西山 大輔 on 2016/02/11.
+//  Copyright (c) 2016年 maplesystems. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
